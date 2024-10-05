5ad7f3741914ff42393d4c7e5770761d2262b11f
Fosiak <maticraf580@gmail.com>
2024-10-06 01:15:01 +0200

    COMMIT

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello vercel!
+          Hello vercel!!
         </p>
         <a
           className="App-link"
